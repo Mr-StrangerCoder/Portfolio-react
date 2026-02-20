867a2d6457dc167d9f4b0d21cd435cb6bb812e7a
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-02-20 09:16:10 +0530

    update aboutme.jsx

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -3,17 +3,27 @@ import './about.css'
 
 
 const AboutMe = () => {
+  let about = {
+    name: 'Aadesh Vasantrao Sonawane',
+    dob: '17 July 1997',
+    address: 'R7 Life Republic Marunji-Kasarsai Road, near Marunji Village, Hinjawadi, Pune, Maharashtra 411057 ',
+    
+
+
+  }
   return (
     <body className='about text-white conatiner'>
-      <div className='container-fluid border w-75 mt-5 opacity-box ' style={{ height: '31rem' }}>
+      <div className='container-fluid  bg-dark w-75 mt-5' style={{ opacity: "0.8", borderRadius: '2rem' }}>
         <div className='row p-5'>
           <div className='col-12 col-md-3 border w-25'style={{marginLeft:'3rem', marginTop:'5rem'}}>
             <img src="" alt="" />
             <p>nhk</p>
           </div>
           <div className='col-12 col-md-9 border w-50 ' style={{marginLeft:'10rem'}}>
-            <h1 className='text-white'>hello</h1>
-            <b>Date of Birth:</b>- 17 July 1997 
+            <h1 className='text-white'>{about.name}</h1>
+            <p>{about.dob}</p>
+            <p>{about.address}</p>
+
 
           </div>
         </div >
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -4,7 +4,7 @@ import './skills.css'
 const Skills = () => {
 
   const skill = {
-    html: 80,
+    html:80,
     css: 80,
     js: 70,
     bt: 60,
@@ -15,7 +15,6 @@ const Skills = () => {
   }
   return (
     <body className='skills text-white conatiner'>
-
       <div className='ps-5 w-75 mx-auto mt-3 bg-dark pt-3'style={{ opacity: "0.8", borderRadius: '2rem'}}>
         <h1 className='text-center fw-bold ' style={{ color: '#3daee9' }}>My Skills</h1>
         <div className='row mt-4'>
