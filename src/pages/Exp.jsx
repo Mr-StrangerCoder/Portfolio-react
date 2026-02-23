8730a0b7a90cd74cc816158b9c31bcf0908f8ade
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-02-23 15:22:20 +0530

    add experience page

--- src/pages/Exp.jsx
+++ src/pages/Exp.jsx
@@ -4,8 +4,20 @@ import './exp.css'
 const Exp = () => {
   return (
     <body className='exp text-white conatiner'>
-      <h1 className='text-white'>hello</h1>
-      <p>jsdhjashdjhjhjh</p>
+      <div className='border conatiner w-50  mt-5 py-3' style={{marginLeft:'10rem'}}>
+        <ul class="list-group list-style-type: disc fs-6">
+          <li class="fw-bold mb-3">Completed a 6-month internship at a Soft Skills company, assisting in communication and personality development training programs.</li>
+          <li class="">A list item</li>
+          <li class="">A list item</li>
+          <li class="">A list item</li>
+          <li class="">A list item</li>
+          <li class="">A list item</li>
+     
+
+        </ul>
+      </div>
+
+
 
     </body>
   )
