d7f446d54ca4b7d0b6f63deb2076d68942a4afca
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-03-18 13:23:41 +0530

    update

--- src/pages/Exp.jsx
+++ src/pages/Exp.jsx
@@ -4,14 +4,10 @@ import './exp.css'
 const Exp = () => {
   return (
     <body className='exp text-white conatiner'>
-      <div className='border conatiner w-50  mt-5 py-3' style={{marginLeft:'10rem'}}>
+      <div className=' conatiner w-50  mt-5 py-3' style={{marginLeft:'10rem'}}>
         <ul class="list-group list-style-type: disc fs-6">
-          <li class="fw-bold mb-3">Completed a 6-month internship at a Soft Skills company, assisting in communication and personality development training programs.</li>
-          <li class="">A list item</li>
-          <li class="">A list item</li>
-          <li class="">A list item</li>
-          <li class="">A list item</li>
-          <li class="">A list item</li>
+          <li class="fw-bold mb-3">Completed a 6-month internship as web developer at a Soft Skills company, assisting in communication and personality development training programs.</li>
+          
      
 
         </ul>
